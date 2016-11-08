469b13403f1c6c21e378677029e114605e83c465
AVELL\Diego <diegomazala@gmail.com>
2016-11-08 00:02:45 -0200

    Bug fix in HC_Filter

--- src/laplacian_filter/laplacian_filter.cpp
+++ src/laplacian_filter/laplacian_filter.cpp
@@ -44,20 +44,30 @@ void hc_filter(
 	aiVector3D* dst_vertices,
 	uint32_t number_of_vertices,
 	ai_real alpha, 
-	ai_real beta)
+	ai_real beta, 
+	uint32_t iterations)
 {
+	std::cout << "alpha beta it " << alpha << ' ' << beta << ' ' << iterations << std::endl;
+	aiVector3D* in_verts = new aiVector3D[number_of_vertices];
+	std::memcpy(in_verts, src_vertices, sizeof(aiVector3D) * number_of_vertices);
+
+
 	//
 	// Computing laplacian filter
 	// 
-	aiVector3D* laplacian_verts = new aiVector3D[number_of_vertices];
-	laplacian_filter(vertices_adj, src_vertices, laplacian_verts, number_of_vertices);
+	for (uint32_t it = 0; it < iterations; ++it)
+	{
+		laplacian_filter(vertices_adj, in_verts, dst_vertices, number_of_vertices);
+		std::memcpy(in_verts, dst_vertices, sizeof(aiVector3D) * number_of_vertices);
+	}
+
 
 	//
 	// Computing diff
 	// 
-	aiVector3D* diff_verts = new aiVector3D[number_of_vertices];
+	aiVector3D* bs = new aiVector3D[number_of_vertices];
 	for (uint32_t i = 0; i < number_of_vertices; ++i)
-		diff_verts[i] = laplacian_verts[i] - (alpha * src_vertices[i] + ((ai_real)1.0 - alpha) * src_vertices[i]);
+		bs[i] = dst_vertices[i] - (alpha * src_vertices[i] + ((ai_real)1.0 - alpha) * src_vertices[i]);
 
 
 	//
@@ -66,18 +76,17 @@ void hc_filter(
 	int i = 0;
 	for (const std::vector<uint32_t> adj : vertices_adj)
 	{
-		aiVector3D& out_vert = dst_vertices[i];
 		aiVector3D d(0, 0, 0);
 		for (const uint32_t vi : adj)
-			d += diff_verts[vi]; // sum of adjacent vertices (all inclusive)
+			d += bs[vi]; // sum of adjacent vertices (all inclusive)
 
-		out_vert = laplacian_verts[i] - (beta * diff_verts[i] + (((ai_real)1.0 - beta) * ((ai_real)1.0 / adj.size())) * d);
+		dst_vertices[i] -= beta * bs[i] + (((ai_real)1.0 - beta) / adj.size()) * d;
 
 		i++;
 	}
 	
-	delete[] laplacian_verts;
-	delete[] diff_verts;
+	delete[] in_verts;
+	delete[] bs;
 }
 
 
@@ -95,16 +104,18 @@ int main(int argc, char* argv[])
 	const std::string output_format = (argc > 2) ? argv[2] : "obj";
 	const uint32_t iterations = (argc > 3) ? atoi(argv[3]) : 1;
 	const bool use_hc_filter = (argc > 4);
-	const ai_real alpha = (ai_real)(argc > 5) ? atoi(argv[5]) : 0.0f;
-	const ai_real beta = (ai_real)(argc > 6) ? atoi(argv[6]) : 0.5f;
+	const ai_real alpha = (ai_real)((argc > 4) ? atof(argv[4]) : 0.0f);
+	const ai_real beta = (ai_real)((argc > 5) ? atof(argv[5]) : 0.5f);
 	
 	//
 	// Composing output file name
 	// 
+	std::stringstream hc_ss;
+	hc_ss << "_hc_" << alpha << '_' << beta;
 	std::stringstream ss;
 	ss << input_filename.substr(0, input_filename.size() - 4)
-		<< ((use_hc_filter) ? "_hc_" : "_")
-		<< iterations << '.' << output_format;
+		<< hc_ss.str()
+		<< '_' << iterations << '.' << output_format;
 	std::string output_filename = ss.str();
 
 
@@ -167,9 +178,9 @@ int main(int argc, char* argv[])
 	aiVector3D* vertices_output = new aiVector3D[mesh->mNumVertices];
 	if (use_hc_filter)
 	{
-		for (uint32_t it = 0; it < iterations; ++it)
+		//for (uint32_t it = 0; it < iterations; ++it)
 		{
-			hc_filter(vertices_adj, mesh->mVertices, vertices_output, mesh->mNumVertices, alpha, beta);
+			hc_filter(vertices_adj, mesh->mVertices, vertices_output, mesh->mNumVertices, alpha, beta, iterations);
 			std::memcpy(mesh->mVertices, vertices_output, sizeof(aiVector3D) * mesh->mNumVertices);
 		}
 	}
